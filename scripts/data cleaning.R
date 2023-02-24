f6f98ac5d320b77730efc057da3efb20fd47373a
fanmin <Cainefm@gmail.com>
2023-02-24 11:30:29 +0800

    add shrink; stop after shrinking interval

--- scripts/data cleaning.R
+++ scripts/data cleaning.R
@@ -20,14 +20,31 @@ library(data.table) # data cleaning
 library(pbapply) #progress bar
 library(lubridate)
 
+shrink_interval <- function(data,idc,st,ed,gap=1){
+    data <- copy(data[,.(id=get(idc),
+                         st=lubridate::ymd(get(st)),
+                         ed=lubridate::ymd(get(ed)))])
+    setorder(data,id,st)
+    output <- data[,indx:=c(0, cumsum(as.numeric(shift(st,type="lead"))>
+                                          cummax(as.numeric(ed)+gap))[-.N]),id
+    ][,.(st=min(st),ed=max(ed)),.(id,indx)]
+    setnames(output,c("st","ed"),c(st,ed))
+    return(output)
+}
+
 new_cohort <- pblapply(list.files(pattern="conv.xlsx","M:/Cohort Raw Data (do not edit)/Stroke and PPI/cdars converted data/stroke dx",full.names = T),readxl::read_excel)
 new_cohort <- rbindlist(new_cohort)
 colnames(new_cohort) <- stringr::str_replace_all(gsub("\n","",colnames(new_cohort)), "[^[:alnum:]]", "_")
+new_cohort[,Patient_Type__IP_OP_A_E_:=gsub("\n","",Patient_Type__IP_OP_A_E_)]
+new_cohort[Patient_Type__IP_OP_A_E_ %in% c("I","A"),.SD[Reference_Key],Reference_Key]
+
 
 
 new_ip <- pblapply(list.files(pattern="conv.xlsx","M:/Cohort Raw Data (do not edit)/Stroke and PPI/cdars converted data/stroke ip/",full.names = T),readxl::read_excel)
 new_ip <- rbindlist(new_ip)
 colnames(new_ip) <- stringr::str_replace_all(gsub("\n","",colnames(new_ip)), "[^[:alnum:]]", "_")
+new_ip[,Principal_Diagnosis_Code:=gsub("\n","",Principal_Diagnosis_Code)]
+new_ip[grepl('433.01|^433.11|^433.21|^433.31|^433.81|^433.91|^434|^436|^437.0|^437.1|^430|^431|^432',Principal_Diagnosis_Code),.SD[ymd(Admission_Date__yyyy_mm_dd_)==min(Admission_Date__yyyy_mm_dd_)],Reference_Key]
 
 colnames(stroke)
 
@@ -69,17 +86,66 @@ opppi_stroke_oralclean <- opppi_stroke_oralclean[!Reference_Key %in% rm_all$Refe
 pbppi0314_rm <- rbind(ppi_pb_0312,ppi_pb_1314)[Type_of_Patient__Drug_ %in% c("I","D") | !Route %in% c("ORAL","PO") | Route==""]
 ppi_ip_0314 <- pbppi0314_rm[Reference_Key %in% opppi_stroke_oralclean$Reference_Key] #1064229
 
-
 opppi_stroke_oralclean[, min_op:=min(Prescription_Start_Date), Reference_Key]
 
-merge(opppi_stroke_oralclean,
-      ppi_ip_0314[,.(Reference_Key,Prescription_Start_Date)],
-      by="Reference_Key",allow.cartesian = T)
+
+ip_op <- merge(opppi_stroke_oralclean,
+      ppi_ip_0314[,.(Reference_Key,ip_rxst=Prescription_Start_Date)],
+      by="Reference_Key",all.x=T,allow.cartesian = T)
+
+ip_op2 <- ip_op[!is.na(ip_rxst) & ymd(ip_rxst)<=ymd(min_op)]
 
 
+opppi_stroke_oralclean_3 <- opppi_stroke_oralclean[!Reference_Key %in% ip_op2$Reference_Key]
+#127071
+
+ppi_ip_0314_RC <- ppi_ip_0314[!Reference_Key %in% ip_op2$Reference_Key]
+#178387
+ppi_ip_0314_RC[,IP_RC:=min(Prescription_Start_Date),Reference_Key]
+
+# /*key LC data into the dataset*/
+
+pat_lc <- as.data.table(haven::read_sas("./data/SAS raw files/pat_lc.sas7bdat"))
+
+opppi_stroke_oralclean_3 <- merge(opppi_stroke_oralclean_3,pat_lc,by="Reference_Key") # 127071
+
+# /*key right censored IP PPIs/other routes into the dataset*/
+
+opppi_stroke_oralclean_3 <- merge(opppi_stroke_oralclean_3,
+                                  unique(ppi_ip_0314_RC[,.(Reference_Key,IP_RC)]),
+                                  by="Reference_Key",all.x=T)#127071
+
+opppi_stroke_oralclean_3[,`:=`(lcl2=min_lc+365,
+                               y1=ymd("20030101"),
+                               y2=ymd("20141231"))
+                         ][,`:=`(st_st=pmax(ymd(Date_of_Birth__yyyy_mm_dd_),y1,na.rm = T),
+                                 st_en=pmin(ymd(Date_of_Registered_Death),ymd(y2),ymd(IP_RC),na.rm = T))]
+
+remove <- opppi_stroke_oralclean_3[st_st>st_en | ymd(Prescription_End_Date)< st_st]
+
+opppi_stroke_oralclean_3 <- opppi_stroke_oralclean_3[ymd(Prescription_Start_Date)<=st_en]#59766
+
+opppi_stroke_oralclean_3 <- opppi_stroke_oralclean_3[!Reference_Key %in% remove$Reference_Key]
+
+oralclean_coh1 <- opppi_stroke_oralclean_3
+
+# after 30 days overlapping
+remove <- oralclean_coh1[st_st>st_en | ymd(Prescription_End_Date)<st_st]
+#0
+oralclean_coh1 <- oralclean_coh1[ymd(Prescription_Start_Date)<=st_en]
+
+oralclean_coh1 <- oralclean_coh1[!Reference_Key %in% remove$Reference_Key]
+
+oralclean_coh1[ymd(Prescription_Start_Date)< st_st & ymd(Prescription_End_Date)>= st_st,]
+oralclean_coh1[ymd(Prescription_End_Date)> st_en & ymd(Prescription_Start_Date)<= st_en,]
 
 
-a
+abcd <- shrink_interval(oralclean_coh1,id = "Reference_Key",
+                st = "Prescription_Start_Date",ed = "Prescription_End_Date",gap=30)
+
+as.data.table(merge(as.data.frame.table(table(oralclean_coh1$Reference_Key)),as.data.frame.table(table(abc$Reference_Key)),by="Var1"))[!Freq.x==Freq.y]
+as.data.table(merge(abcd[,.N,id][,setnames(.SD,"id","Reference_Key")],
+                    abc[,.N,Reference_Key],by="Reference_Key"),all=T)[!N.x==N.y]
 
 # cohort ------------------------------------------------------------------
 dx <- haven::read_sas("./data/SAS raw files/allstroke_date.sas7bdat") # 49308 records
